248dea0d94e792eae1c6fedf3d5270cf005c3e15
HuyHoangg1409 <hoangdilamviec@gmail.com>
2026-05-16 22:12:11 +0700

    Demo Minimax

--- ChessAI.py
+++ ChessAI.py
@@ -40,6 +40,70 @@ class ChessAI:
         return score
     
 
+    # ==========================
+    # THUẬT TOÁN MINIMAX
+    # ==========================
+    def minimax(self, depth, is_maximizing):
+        """
+        Thuật toán Minimax đệ quy
+        depth: độ sâu còn lại cần tính
+        is_maximizing: True nếu là lượt của Trắng, False nếu là Đen
+        """
+        # Điều kiện dừng: đạt độ sâu tối đa hoặc game kết thúc
+        if depth == 0 or self.board.is_game_over():
+            return self.evaluate_board()
+        
+        if is_maximizing:
+            max_evaluate = -float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                evaluate = self.minimax(depth - 1, False) #Đệ quy và đổi lượt
+                self.board.pop()
+                max_evaluate = max(max_evaluate, evaluate)
+            return max_evaluate
+        else:
+            min_evaluate = float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                evaluate = self.minimax(depth - 1, True) #Đệ quy và đổi lượt
+                self.board.pop()
+                min_evaluate = min(min_evaluate, evaluate)
+            return min_evaluate
+        
+
+    def get_minimax_move(self, depth=3):
+        """Để mặc định độ sâu là 3"""
+        best_move = None
+
+        if self.board.turn == chess.WHITE:
+            best_value = -float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                value = self.minimax(depth, False)
+                self.board.pop()
+                
+                if value > best_value:
+                    best_value = value
+                    best_move = move
+        
+        else:
+            best_value = float('inf')
+            for move in self.board.legal_moves:
+                self.board.push(move)
+                value = self.minimax(depth, True)
+                self.board.pop()
+
+                if value < best_value:
+                    best_value = value
+                    best_move = move
+        
+        return best_move
+
+
+
+    # ==========================
+    # THUẬT TOÁN THAM LAM (GREEDY)
+    # ==========================
     def get_best_move(self):
         """Tìm nước đi tốt nhất theo cách tham lam(chỉ nhìn trước được 1 bước)"""
         best_move = None
